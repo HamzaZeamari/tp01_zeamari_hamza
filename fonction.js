1f9be5a2453570c58af6b1a9f5fc6cb5f826230b
HamzaZeamari <zeam.hamza@gmail.com>
2021-09-12 18:30:09 +0200

    update

--- fonction.js
+++ fonction.js
@@ -1,7 +1,53 @@
-function funcValidation(){
-    if($("#nom").value == ''){
-        
-    }else{
-
-    }
-}
+$(document).ready(function() {
+    $('#nom').change(function() {
+        let characterCount = $(this).val().length;
+        if (characterCount >= 3) {
+            $('#nomHelp').hide();
+        } else {
+            $('#nomHelp').show();
+        }
+    });
+})
+$(document).ready(function() {
+    $('#prenom').change(function() {
+        let characterCount = $(this).val().length;
+        if (characterCount >= 3) {
+            $('#prenomHelp').hide();
+        } else {
+            $('#prenomHelp').show();
+        }
+    });
+})
+$(document).ready(function() {
+    $('#CP').change(function() {
+        let characterCount = $(this).val().length;
+        console.log(characterCount);
+        if (characterCount == 4 || characterCount == 5) {
+            $('#cpHelp').hide();
+        } else {
+            $('#cpHelp').show();
+        }
+    });
+})
+$(document).ready(function() {
+    $('#ville').change(function() {
+        let characterCount = $(this).val().length;
+        console.log(characterCount);
+        if (characterCount > 0) {
+            $('#villeHelp').hide();
+        } else {
+            $('#villeHelp').show();
+        }
+    });
+})
+$(document).ready(function() {
+    $('#tel').change(function() {
+        let characterCount = $(this).val().length;
+        console.log(characterCount);
+        if (characterCount = 10) {
+            $('#telHelp').hide();
+        } else {
+            $('#telHelp').show();
+        }
+    });
+})
